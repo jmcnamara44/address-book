952cd1131fd935a712a9d3a3ed862bf324e32ed0
Jimmy McNamara and Shirley Dennis <shirley@>
2018-04-02 15:02:48 -0700

    Add functionality to take user input and turn into list item.

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+function Contact(first,last,street,city,state) {
+  this.firstName = first;
+  this.lastname = last;
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedStreet = $("input#new-street").val();
+    var inputtedCity = $("input#new-city").val();
+    var inputtedState = $("input#new-state").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedStreet,inputtedCity,inputtedState);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+
+
+  });
+
+});
