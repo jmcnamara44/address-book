ea0f1cdd41174d3304148057d699368db82d36bf
Jimmy McNamara and Shirley Dennis <jimmy@>
2018-04-02 15:42:13 -0700

    Add css styling and additional js functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
+
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var inputtedStreet = $("input#new-street").val();
@@ -20,6 +21,12 @@ $(document).ready(function() {
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
 
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
 
   });
 
